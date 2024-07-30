662be196ff3f924ca4f26e9a1b6829e537c40d57
CopyandPaste-png <l.irsigler64@gmail.com>
2024-07-30 17:24:52 +0200

    Added ability to create/join lobbies.
    Start game also added

--- public/js/client.js
+++ public/js/client.js
@@ -1,11 +1,39 @@
 var socket = io.connect();
 
-socket.on('connect', function() {
-    console.log(socket.id)
+var roomID;
+var playerID;
+var currRoom;
+var host;
+var levelRecieved;
+var myCharacterNum;
+
+socket.emit('connection',()=>{
+    playerID = socket.id;
 });
 
 
-socket.on("hello",(message)=>{
-    console.log(message)
+socket.on("getRoomCode", (room) => {
+    console.log(playerID);
+    roomID = room.roomCode;
 })
 
+socket.on("getRoomError",(message)=>{console.log(message)})
+
+socket.on("playerJoined",(playerID)=>{console.log(playerID)})
+
+socket.on("roomInfo",(room)=>{
+    currRoom = room;
+    updateList(currRoom)
+})
+
+socket.on("startGame",(data)=>{
+    console.log("Game started");
+    levelRecieved = data["level"];
+    currRoom = data["room"];
+    startGame()
+})
+
+
+socket.on("recievePlayerInfo",(playerInfo)=>{
+    myCharacterNum = playerInfo.myCharacterNum;
+})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,88 @@
+const misc = require('./misc');
+const level = require('./levels');
+
+let rooms = [];
+
 module.exports = function(io) {
     io.on('connection', (socket) => {
         const sessionID = socket.id;
         console.log(`User ${sessionID} connected`);
 
-        let message = `CONNECTED ${sessionID}`;
-
-        io.emit('hello',message ); 
-
         socket.on('disconnect', () => {
+            const roomsWithPlayer = rooms.filter(room => room.room.players.includes(sessionID));
             console.log(`User ${sessionID} disconnected`);
+        });
+
+        socket.on("createLobby", () => {
+            let room = {
+                roomCode:misc.makeid(4),
+                players:[],
+                gameStarted:false
+            }
+
+            room.players.push(sessionID);
+            rooms.push({id:room.roomCode, room:room});
+            socket.join(room.roomCode);
+            socket.emit("getRoomCode", room);
+            io.in(room.roomCode).emit("roomInfo",room);
+        });
+
+        socket.on("joinLobby",(roomCode)=>{
+            let index = rooms.findIndex((room)=>room.id === roomCode)
+            let foundRoom = rooms[index];
+
+            if (!foundRoom){
+                let message = "Room not found";
+                socket.emit("getRoomError", message);
+                return;
+            }
+
+            if (foundRoom.room.players.length >= 4){
+                let message = "Room is full";
+                socket.emit("getRoomError", message);
+                return;
+            }
+
+            foundRoom.room.players.push(sessionID);
+            rooms[index]=foundRoom;
+
+            let playerID = `${sessionID}`;
+            socket.join(foundRoom.id);
+            io.in(foundRoom.id).emit("playerJoined",playerID)
+            io.in(foundRoom.id).emit("roomInfo",foundRoom.room);
+        })
+
+        socket.on("startGame",(roomID)=>{
+            let index = rooms.findIndex((room)=>room.id === roomID)
+            let foundRoom = rooms[index];
+
+            if (!foundRoom){
+                let message = "Room not found";
+                socket.emit("getRoomError", message);
+                return;
+            }
+
+            foundRoom.room.gameStarted = true;
+            rooms[index]=foundRoom;
+
+            const roomToSend = foundRoom.room;
+            const levelToSend = level.getLevel("level2");
+            io.in(foundRoom.id).emit("startGame",{level:levelToSend,room:foundRoom.room});
+
+            for (let i = 0; i < roomToSend.players.length; i++){
+                let playerInfo = {
+                    myCharacterNum:i
+                }
+                io.to(roomToSend.players[i]).emit("recievePlayerInfo",playerInfo);
+            }
+        })
+
+
+        socket.on("startGameUpdatePlayer",(roomCode,playerID)=>{
+            console.log(roomCode,playerID);
         });
       });
 
-    
+
+
 };
