a9c7b623bea9d340b7ca81782cb7801a95316797
CopyandPaste-png <l.irsigler64@gmail.com>
2024-07-30 17:27:05 +0200

    Added more event handlers

--- public/js/render.js
+++ public/js/render.js
@@ -5,6 +5,28 @@ var ctx = canvas.getContext("2d");
 const gravity = 2;
 let prevDirection = null;
 
+
+//Event listeners
+addEventListener('keypress',(event)=>{
+    if (event.key=='w' ||event.key=="d"||event.key=="a"||event.key=="s"){
+        prevDirection = event.key
+    }else if(event.key=="1" || event.key=="2"||event.key=="3"||event.key=="4"){
+        hotBar.changeSelection(parseInt(event.key));
+    }
+})
+
+addEventListener('keydown',(event)=>{
+    if (event.key=='w' ||event.key=="d"||event.key=="a"||event.key=="s"){
+        prevDirection = event.key
+    }
+})
+
+
+addEventListener('mousedown',(event)=>{
+})
+
+
+//Collision detection
 function boxCollision(object1,object2){
     if(
         object1.x+object1.width >= object2.x&&
@@ -28,14 +50,11 @@ function boxCollision(object1,object2){
         );
 
         if (minDepth==collisionDepths.top){
-            console.log("top")
             object1.speedY+=2
             object1.y+=5
         }else if (minDepth==collisionDepths.bottom){
-            console.log("bottom")
             object1.y-=5
         }else if(minDepth==collisionDepths.left){
-            console.log("left")
             object1.x+=10
             object1.invertDirection()
         }else{
@@ -46,17 +65,17 @@ function boxCollision(object1,object2){
     }
     return false;
 }
-//const circle = new Circle(canvas,20,0,2*Math.PI,"red",4,"black");
-//circle.draw(100,100)
 
-level = getLevel("level2",canvas.height,canvas.width);
+
+//Receive level from server
+let level = updateLevelJSON(levelRecieved,canvas.height,canvas.width);
 platformInfo=level["platformInfo"];
 platformLocations=level["platformLocations"];
-
-console.log(level)
+spawnLocation = level["spawnLocations"][myCharacterNum];
+console.log(canvas.width,canvas.height);
+console.log(spawnLocation)
+//Add platforms
 platforms = []
-
-
 platformInfo.forEach((element,i)=>{
     let newPlatform = new Platform(canvas,element.width,element.height,"black",element.horizontal);
     platforms.push(newPlatform);
@@ -64,27 +83,27 @@ platformInfo.forEach((element,i)=>{
     newPlatform.draw(loc.x,loc.y);
 })
 
+//Add hotbar
+const hotBar = new Hotbar(canvas,200,50);
 
-const sprite = new Sprite(canvas,48,48,"./img/character2.png",2)
+
+let spirtes = [];
+
+const sprite = new Sprite(canvas,48,48,"./img/character2.png",myCharacterNum)
 
 sprite.img.onload = ()=> {
-    sprite.x = 10;
-    sprite.y =10
+    sprite.x = spawnLocation.x;
+    sprite.y = spawnLocation.y;
 }
 
 
-addEventListener('keypress',(event)=>{
-    if (event.key=='w' ||event.key=="d"||event.key=="a"||event.key=="s"){
-        prevDirection = event.key
-    }
-})
 
-addEventListener('mousedown',(event)=>{
-
-})
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    hotBar.draw();
+    
+
     platforms.forEach((platform)=>{
         platform.redraw();
     })
@@ -94,14 +113,13 @@ function animate(){
     })
     //check if in air, if in air, then apply gravity
     sprite.ground = platforms.some((platform)=>{
-        //console.log(sprite.y-platform.y)
         return (sprite.x>platform.x && sprite.x<platform.x+platform.width &&sprite.y-platform.y<0 && sprite.y-platform.y>-50 )
     })
 
     sprite.update(prevDirection)
     sprite.draw(prevDirection)
 
-    prevDirection=null
+    prevDirection=null;
     requestAnimationFrame(animate);
 
 }
