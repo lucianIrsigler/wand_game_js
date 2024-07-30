3756b08960a458fe2a4117e7c0b22b6eace6d91e
CopyandPaste-png <l.irsigler64@gmail.com>
2024-07-30 17:27:45 +0200

    Added spacing/ res for /game

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express")
 const {createServer} = require("node:http")
 
 const app = express()
+
 app.use(express.static("public"))
+
 const server = createServer(app)
+
 var io = require('socket.io')(server,{
     connectionStateRecovery: {}
 });
@@ -11,6 +14,12 @@ var io = require('socket.io')(server,{
 app.get("/",(req,res)=>{
     res.sendFile("public/index.html")
 })
+
+app.get("/game",(req,res)=>{
+    res.sendFile("game.html",{root:"public"})
+})
+
+
 
 
 require('./server/server.js')(io);
